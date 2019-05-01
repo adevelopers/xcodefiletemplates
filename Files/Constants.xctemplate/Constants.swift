561abe8f8acf86c15418524bfe5bad26602c4b2f
Kirill Khudyakov <hudiakov@gmail.com>
2019-05-01 10:04:34 +0300

    Add template Constants

---
+++ Files/Constants.xctemplate/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  ___COPYRIGHT___
+//
+
+import UIKit
+
+
+enum Constants {
+
+    enum Colors {
+        static let backgroundColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        static let textColor = #colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0)
+    }
+    
+}
